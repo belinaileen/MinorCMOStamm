616de64644a25b9a431b5d37aada1abcd78742b6
belinaileen <aileenbelina@gmail.com>
2025-01-31 00:57:07 +0100

    Update bp-dashboard-dutch.py

--- bp-dashboard-dutch.py
+++ bp-dashboard-dutch.py
@@ -255,7 +255,7 @@ with col0[1]:
     st.markdown("""
         <div style='text-align: left; padding: 10px; display: flex; align-items: center;'>
             <h1 style='color: #e5007d; font-size: 20px; font-weight: bold; margin-top: 0; margin-right: 8px;'>Voor de downloadkaartpagina:</h1>
-            <a href="mapdownload.streamlit.app" target="_blank" style="
+            <a href="https://mapdownload.streamlit.app/" target="_blank" style="
                 text-decoration: none;
                 background-color: #e5007d;
                 color: white;
